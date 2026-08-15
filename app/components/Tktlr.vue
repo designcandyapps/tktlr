257ae1aee09f0f140bd4e70644798eeaa145f95d
Erin Dunn Halligan <erin@designcandy.com>
2026-08-15 15:40:55 -0500

    Create Tktlr.vue

---
+++ app/components/Tktlr.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref,onMounted} from "vue"; const imageUrl=ref(""); const proxyUrl=ref(""); const isLoading=ref(false);
+
+const genTktlr=async()=>{
+  alert(2);
+  proxyUrl.value=`/api/proxy?url=${encodeURIComponent(imageUrl.value)}`;
+  //img.src=proxyUrl.value;
+  alert(proxyUrl.value);
+};
+
+onMounted(()=>{
+  //window.onload=function(){
+    setTimeout(function(){
+      imageUrl.value=document.getElementById("ee").src;
+      genTktlr();
+    },1800);
+  //}
+});
+</script>
