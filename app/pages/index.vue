827d94c88347ed54bc84f079d504e2debba0aea3
Erin Dunn Halligan <erin@designcandy.com>
2026-08-15 15:01:09 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,59 +15,85 @@ async function fetchGetty(query){
   }catch(error){console.error("Error2:",error)}
 }
 onMounted(()=>{
-  setTimeout(function(){
-    const sc1=document.createElement('script'); sc1.src='https://pinfluents.com/_BCK/4/inc/zo/gl.js';
-    const sc2=document.createElement('script'); sc2.src='https://pinfluents.com/_BCK/4/inc/zo/sl2.js';
-    document.head.appendChild(sc1); document.head.appendChild(sc2);
+  //setTimeout(function(){
+    //const pr="cars";
+    //document.getElementById("prompt").value=document.getElementById("tr").innerText;
+    //const pr:string=new URLSearchParams(location.search).get("pr")??document.getElementById("prompt").value;
+    //pr=pr&&pr!="Innovation for the energy of today and tomorrow"?pr:document.getElementById("prompt").value;
+    //alert("PR: "+pr);
 
-    const prompt=document.querySelector("#prompt").value; //alert("PR1: "+prompt);
+    const pr:string=new URLSearchParams(location.search).get("pr")??"design"; //pr=pr&&pr!=""?pr:document.getElementById("prompt").value;
+    document.getElementById("tr").innerText=pr; document.getElementById("prompt").value=document.getElementById("tr").innerText;
+    //alert("PROMPT: "+document.querySelector("#prompt").value);
+    //alert("PRP: "+document.getElementById("tr").innerText);
+
+    //document.getElementById("tr").innerText="design";
+    const prompt=document.querySelector("#prompt").value; //alert("PROMPT2: "+prompt);
+    //const prompt=document.getElementById("tr").innerText; //alert("PROMPT2: "+prompt);
     const pr2=document.querySelector("#pr2").value; //alert("PR2: "+pr2);
     const pr3=document.querySelector("#pr3").value; //alert("PR3: "+pr3);
     const pr4=document.querySelector("#pr4").value; //alert("PR4: "+pr4);
-    const pr5=document.querySelector("#pr5").value; //alert("PR5: "+pr5);
-    //fetchPh(prompt).then(photos=>{photos.forEach(photo=>{pho.value=photo.urls.small})});
-    //fetchGetty(prompt).then(image=>{pho2.value=image.display_sizes[0].uri});
-  },7800)
+    const pr5=document.querySelector("#pr5").value; //alert("PR5: "+pr5);*/
+    fetchPh(prompt).then(photos=>{
+      photos.forEach(photo=>{pho.value=photo.urls.small}); //alert("PH: "+pho.value);
+    });
+    //fetchGetty(prp).then(image=>{pho2.value=image.display_sizes[0].uri});
+  //},7800);
 });
 </script>
 
 <template>
   <ULandingHero v-if="page.hero" v-bind="page.hero">
-    <span class="g">
-      <span id="z"><ColorThief /></span><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
+    <span class="g"><input id="t" v-model="t">
+    <span id="z"><ColorThief /></span><input id="prompt" v-model="prompt"><input id="pho" v-model="pho"><input id="pho2" v-model="pho2">
       <input id="pr2" v-model="pr2"><br><input id="pr3" v-model="pr3"><br><input id="pr4" v-model="pr4"><br><input id="pr5" v-model="pr5">
       <span id="response" v-if="response">{{response}}</span>
     </span>
     <template #title><MDC :value="page.hero.title" /></template><MDC :value="page.hero.code" class="prose prose-primary dark:prose-invert mx-auto" />
   </ULandingHero>
-  <ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /><Slider2 /></UPageGrid></ULandingSection>
+  <ULandingSection :title="page.features.title" :links="page.features.links"><UPageGrid><ULandingCard v-for="(item,index) of page.features.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   <ULandingSection :title="page.sections.title" :links="page.sections.links"><UPageGrid><ULandingCard v-for="(item,index) of page.sections.items" :key="index" v-bind="item" /><Slider2 /></UPageGrid></ULandingSection>
   <ULandingSection :title="page.mid.title" :links="page.mid.links"><UPageGrid><ULandingCard v-for="(item,index) of page.mid.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   <ULandingSection :title="page.bottom.title" :links="page.bottom.links"><UPageGrid><ULandingCard v-for="(item,index) of page.bottom.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
-  <ULandingSection :title="page.lower.title" :links="page.lower.links"><UPageGrid><ULandingCard v-for="(item,index) of page.lower.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
-  <ULandingSection :title="page.lower.title" :links="page.lower.links"><UPageGrid><ULandingCard v-for="(item,index) of page.lower.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
-  <ULandingSection :title="page.lower.title" :links="page.lower.links"><UPageGrid><ULandingCard v-for="(item,index) of page.lower.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
   <ULandingSection :title="page.lower.title" :links="page.lower.links"><UPageGrid><ULandingCard v-for="(item,index) of page.lower.items" :key="index" v-bind="item" /></UPageGrid></ULandingSection>
 </template>
 
 <script lang="ts">
 export default{
-  data(){return{prompt:"",response:null}},
-  mounted(){setTimeout(()=>{this.send()},7800)},
+  data(){return{prompt:"",prp:"",response:null}},
+  mounted(){
+    setTimeout(()=>{this.send3()},7800);
+  },
   methods:{
     async send(){
-      const response=await fetch("/api/tt",{
-        method:"POST",
+      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
+      const data=await response.json(); this.response=data.reply; //alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
+      //alert("T1: "+document.querySelector("#tr").innerText);
+      document.querySelector("#h1n").innerText=this.response;
+      //document.querySelector("#tr").innerText="sdesign";
+    },
+    async send2(){
+      const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#pr2").value})});
+      const data=await response.json(); this.response=data.reply; //alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response);
+      //alert("S: "+document.querySelector(".slick-slide"));
+      document.querySelector(".slick-slide>div>div>div>div").innerText=this.response;
+    },
+    async send3(){
+      const response=await fetch("https://api.tickettailor.com/v1/events",{
         headers:{
-          "Content-Type":"application/json"},
-          body:JSON.stringify({message:document.querySelector("#prompt").value})
+          Accept:"application/json",
+          Authorization:"Basic "+Buffer.from("sk_14995_133548_95cbe0f619ded70f2d57a144acefffc5:").toString("base64")},
         });
+        const data=await response.json();
+        alert("Test1");
+        //alert("RES1P: "+JSON.stringify(data));
+        return data;
+    },
+    async send4(){
+      const response=await fetch("/api/tt",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#pr2").value})});
       const data=await response.json();
-      this.response=data.reply;
-      alert("RES00: "+JSON.stringify(data));
-      //alert("RES01: "+this.response);
-      //console.log(data.message.content);
-      //document.querySelector("#t").innerText=this.response;
+      alert("Test2");
+      this.response=data.reply; //alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response);
     },
   },
 }
