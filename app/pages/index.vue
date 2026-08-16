8a2bda50d7b02e6ead1a59c70506c5539c494824
Erin Dunn Halligan <erin@designcandy.com>
2026-08-16 11:14:42 -0500

    Refactor send2 method to remove alerts
    Removed alert for response data and kept the response assignment.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -80,7 +80,7 @@ export default{
     },
     async send2(){
       const response=await fetch("/api/chat",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({message:document.querySelector("#prompt").value})});
-      const data=await response.json(); this.response=data.reply; alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
+      const data=await response.json(); this.response=data.reply; //alert("RES00: "+JSON.stringify(data)); alert("RES01: "+this.response); //console.log(data.message.content);
       //alert("T1: "+document.querySelector("#tr").innerText);
       document.querySelector("#h1n").innerText=this.response;
       //document.querySelector("#tr").innerText="sdesign";
